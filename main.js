a37d9a8771ffdcafdead5d2c85461c472272431f
AFCSR <artforcausebyshalinireddy@gmail.com>
2023-11-29 22:25:57 +0530

    Added Hero

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ menu_btn.addEventListener('click', () => {
         menu.classList.add('opened');
         menu.style.marginLeft = '0%';
         overlay.classList.remove('hidden');
-        
+
         overlay.addEventListener('click', () => {
             menu.classList.remove('opened');
             menu_btn.classList.remove('opened');
@@ -20,3 +20,40 @@ menu_btn.addEventListener('click', () => {
         overlay.classList.add('hidden');
     }
 });
+
+
+function scrollTrigger(selector, options = {}) {
+    let els = document.querySelectorAll(selector)
+    els = Array.from(els)
+    els.forEach(el => {
+        addObserver(el, options)
+    })
+}
+function addObserver(el, options) {
+    if (!('IntersectionObserver' in window)) {
+        if (options.cb) {
+            options.cb(el)
+        } else {
+            entry.target.classList.add('active')
+        }
+        // We don't need to execute the rest of the code
+        return
+    }
+
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                if (options.cb) {
+                    options.cb(el)
+                } else {
+                    entry.target.classList.add('active')
+                }
+                observer.unobserve(entry.target)
+            }
+        })
+    }, options)
+    observer.observe(el)
+}
+
+heroText = document.querySelector('.hero-text');
+scrollTrigger('.hero-text')
